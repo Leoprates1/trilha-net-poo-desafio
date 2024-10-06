5bd67bb88b16a3db48c913e55756935657412b7c
Leoprates1 <leumafia@gmail.com>
2024-10-06 16:49:05 -0300

    Desafia concluido

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,17 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        // Construtor que passa parâmetros para a classe base
+        public Iphone(string numero, string modelo, string imei, int memoria)
+            : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        // Sobrescrita do método abstrato
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no iPhone.");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,25 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Testando a classe Nokia
+        Smartphone nokia = new Nokia("123456", "Nokia 3310", "111111111111111", 64);
+        Console.WriteLine("### Teste com Nokia ###");
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine();
+
+        // Testando a classe iPhone
+        Smartphone iphone = new Iphone("654321", "iPhone 13", "222222222222222", 128);
+        Console.WriteLine("### Teste com iPhone ###");
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+
+        Console.WriteLine("Testes concluídos.");
+    }
+}
